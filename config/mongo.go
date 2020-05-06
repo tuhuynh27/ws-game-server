9d93f82949863ccb3cff8fd278edba8815b9e86b
Lê Đặng Hoài Nam <namhoai95@gmail.com>
2020-05-06 19:24:23 +0700

    Use ReadInConfig function (#3)
    Co-authored-by: Nam le <nam.le2@be.com.vn>

--- config/mongo.go
+++ config/mongo.go
@@ -2,12 +2,13 @@ package config
 
 import (
 	"context"
+	"log"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
 )
 
-func NewMongoConnection(host, databaseName string) *mongo.Database {
+func (c *Config) NewMongoConnection(host, databaseName string) *mongo.Database {
 	client, err := mongo.NewClient(options.Client().ApplyURI(host))
 	if err != nil {
 		log.Panic(err)
